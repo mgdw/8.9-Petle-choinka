80f35cb69f43361678124462cb18d311bb6f6757
Magda <magwalasek@gmail.com>
2016-11-27 20:55:43 +0100

    Correct mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 
 function rysujChoinkę(n) {
 	var n;
-	var star='*'
-	var space=' '
-	var s=' ';
+	var star='*';
+	var space=' ';
 	for (var j=0; j<=n-1; j++) {
-		space=s+space;
+		space=' '+space;
 	}
 	star=space+star;
 	for (var i=0; i<=n-1; i++) {
 		console.log(star);
 		star +='**';
 		star = star.slice(1, star.length);
-		}
 	}
+}
 h = prompt('Podaj wysokość choinki'),
 console.log(rysujChoinkę(h));
+
+function zaalertuj() {
+    alert('Wyświetlam się jednokrotnie po 2 sekundach.');
+}
+
+var opoznioneWywolanie = setTimeout(zaalertuj, 2000);
